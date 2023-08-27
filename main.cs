2cfd4c24a4b5c9bc20213fbf07f3e7a67af7cbd7
konkeror <sid.mkjee@gmail.com>
2023-08-27 16:02:48 +0200

    Add example of data type usage

---
+++ main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataTypes;
+
+namespace C_Sharp_Examples
+{
+    class TestClass
+    {
+        static void Main()
+        {
+            // Display the number of command line arguments.
+            Console.WriteLine("Hi");
+            DataTypes.DataTypes.data_type_func();
+        }
+    }
+}
+
