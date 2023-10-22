ee8e02e8babee57ac9a0a361091d10b7babfd56d
konkeror <sid.mkjee@gmail.com>
2023-10-22 19:40:15 +0200

    Rearranged files

--- main.cs
+++ main.cs
@@ -1,14 +1,4 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using C_Sharp_Examples.Polymorphism;
-//using DataTypes;
-using PolymorphismExample;
-using OverrideExample;
-
-namespace C_Sharp_Examples
+﻿namespace C_Sharp_Examples
 {
     class TestClass
     {
