9258eaca361b022d79f4e74096690bc093911319
konkeror <sid.mkjee@gmail.com>
2024-03-20 14:13:33 +0100

    Add inheritance failure example

---
+++ Examples/src/Modifiers/Sealed/SealedClassInheritanceFailure.cs
@@ -0,0 +1,31 @@
+﻿// C# code to show restrictions
+// of a Sealed Class
+using System;
+
+namespace Modifiers.Sealed
+{ 
+    class Bird
+    {
+
+    }
+
+    // Creating a sealed class
+    sealed class Test : Bird
+    {
+    }
+
+    // Inheriting the Sealed Class
+    /*
+    class Example : Test // Failure
+    {
+    }
+
+    // Driver Class
+    class Program
+    {
+        public static void MainMethod()
+        {
+        }
+    }
+    */
+}
