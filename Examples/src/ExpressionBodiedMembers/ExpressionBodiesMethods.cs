32324d3ab6d726d82a370227adbe508cddf21dad
konkeror <sid.mkjee@gmail.com>
2026-01-11 13:19:47 +0100

    Expression bodies members as read only property

--- Examples/src/ExpressionBodiedMembers/ExpressionBodiesMethods.cs
+++ Examples/src/ExpressionBodiedMembers/ExpressionBodiesMethods.cs
@@ -24,6 +24,25 @@ namespace C_Sharp_Examples.src.ExpressionBodiedMembers
         public int CalculateAge(int birthYear) => DateTime.Now.Year - birthYear;
         public bool IsOlderThan(int age) => CalculateAge(1990) > age;
         public string FormatName(string format) => format.Replace("{first}", fname).Replace("{last}", lname);
+
+    }
+
+    public class Location
+    {
+
+        /*
+         * The following example defines a Location class whose read-only Name property 
+         * is implemented as an expression body definition that returns the value of the 
+         * private locationName field:
+        */
+        private string locationName;
+
+        public Location(string name)
+        {
+            locationName = name;
+        }
+
+        public string Name => locationName;
     }
 
 
